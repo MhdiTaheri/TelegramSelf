46c61f223e897ef2b920e8255e3f89fa8282dd75
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2024-02-04 14:16:15 +0330

    small change 👀

--- lib/Information.py
+++ lib/Information.py
@@ -6,6 +6,6 @@ api_hash = 'abcdefgh1234'  #<---- api هش شما
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
 
-client = TelegramClient('session_name', api_id, api_hash)
+client = TelegramClient('TRself-MT', api_id, api_hash)
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
