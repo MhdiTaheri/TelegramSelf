cb163f7b43f4c40e298218eb2d1cdcf6b774f386
Mahdi Taheri <151569179+MhdiTaheri@users.noreply.github.com>
2024-02-19 20:16:24 +0330

    helper texts fix 🚀

--- helper.py
+++ helper.py
@@ -65,6 +65,21 @@ async def callback(event):
     elif event.sender_id == admin_user_id and event.data == b"help":
         panel = f'''
         از دستورات زیر استفاده کنید:
+        \n`timename` on|off : زمان در اسم روشن|خاموش
+        \n`mini` on : فونت مینی روشن
+        \n`bio` on|off : بیو روشن|خاموش
+        \n`default` on : فونت عادی
+        \n`bold` on : فونت بولد
+        \n`mono` on : فونت یه مدلی
+        \n`heart` on|off : حالت قلب روشن|خاموش 
+        \n`rname` on|off :  اسم تصادفی روشن|خاموش
+        \n`see rname` : دیدن اسم های تصادفی
+        \n`see bio` : دیدن بیو
+        \n`/addbio` (time,DATE,heart) [text]: این دستور برای اضافه کردن یک بیوگرافی یا توضیح کوتاه به پروفایل شما می‌تواند استفاده شود، که می‌تواند به دیگران در یادگیری بیشتر درباره شما کمک کند.
+        \n`/addlname` (time,heart) [text]: متن تنظیمی برای تایم نیم است و دارای متغییر است که time زمان و heart قلب رندوم قرار میدهد
+        \n`/addrname` [name1,name2,...]: برای تنظیم نام رندوم است
+        \n`/delrname`: برای حذف نام رندوم است
+        \n`see lname` : دیدن متن لست نیم
         \n`/ping` - زمان پاسخ دهی ربات را دریافت کنید.
         \n`/mem` - مصرف حافظه فعلی ربات را دریافت کنید.
         \n`/gmusic` [query] - در SoundCloud برای پیدا کردن یک قطعه موسیقی و دانلود آن جستجو کنید.
@@ -74,21 +89,16 @@ async def callback(event):
         \n/info [@username] - اطلاعاتی درباره کاربری را که آن را مشخص می کنید دریافت کنید.
         \n`/setprof` - عکس پروفایل ربات را (با پاسخ به تصویر) تنظیم کنید.
         \n`/rinfo` - با پاسخ دادن به یکی از پیام های کاربر، اطلاعات درباره کاربر را دریافت کنید.
-        \n`/rem` [num] - حذف پیام‌های اخیر در گروه تا تعداد مشخص شده.
-        \n`/sgoogle` [query] - جستجوی کوتاهی در گوگل برای پیدا کردن پاسخ به سوال شما.
-        \n`/wiki` [query] - جستجوی کوتاهی در ویکی‌پدیای فارسی برای پیدا کردن پاسخ به سوال شما.
-        \n`/save`: این دستور می‌تواند برای ذخیره یک گفتگو یا لاگ چت به یک فایل استفاده شود، که می‌تواند برای نگهداری یک رکورد از بحث‌های مهم یا به اشتراک گذاری با دیگران مفید باشد.
-        \n`/addbio`: این دستور برای اضافه کردن یک بیوگرافی یا توضیح کوتاه به پروفایل شما می‌تواند استفاده شود، که می‌تواند به دیگران در یادگیری بیشتر درباره شما کمک کند.
-        \n`/reload`: این دستور برای بازنشانی رابط گفتگو یا کد ربات استفاده می‌شود، که می‌تواند در صورت وجود هرگونه خطا یا خللی که باید حل شود مفید باشد.
         '''
         back_button = Button.inline("ʙᴀᴄᴋ ⬸", b"Back")
         help_1 = Button.inline("ᴘᴀɢᴇ Ⅰ", b"help")
         help_2 = Button.inline("ᴘᴀɢᴇ Ⅱ", b"help_1")
         help_3 = Button.inline("ᴘᴀɢᴇ Ⅲ", b"help_2")
+        help_4 = Button.inline("ᴘᴀɢᴇ ɪᴠ", b"help_3")
         buttons = [
-            [help_1,help_2,help_3],
+            [help_1,help_2,help_3,help_4],
             [back_button]
-            ]
+        ]
         await event.edit(panel, buttons=buttons)
     elif event.sender_id == admin_user_id and event.data == b"help_1":
         panel = f'''
@@ -96,11 +106,8 @@ async def callback(event):
         \n`/backupchat`: این دستور می‌تواند برای پشتیبان گیری از تمام تاریخچه گفتگو، از جمله پیام‌ها، تصاویر و سایر محتواها استفاده شود، که می‌تواند برای اهداف بایگانی یا در صورت از دست دادن داده‌ها مفید باشد.
         \n`/create_channel`: این دستور برای ایجاد یک کانال یا چت گروهی جدید در داخل پلتفرم پیام رسانی مورد استفاده قرار می‌گیرد، که می‌تواند برای سازماندهی گفتگوها در مورد موضوعات خاص یا با گروه‌های خاص از افراد مفید باشد.
         \n`/calc`: این دستور برای انجام محاسبات ساده مانند جمع، تفریق، ضرب و تقسیم مستقیماً در رابط گفتگو استفاده می‌شود.
-        \n`/tr`: این دستور برای ترجمه متن از یک زبان به زبان دیگر با استفاده از یک سرویس ترجمه آنلاین استفاده می‌شود.
         \n`/silent`:این دستور اگر روی کاربری ریپلی شود کاربر را در لیست سکوت قرار میدهد و اگر در زمان سکوت بیشتر از 5 پیام ارسال کند کاربر را بلاک میکند.
         \n`/unsilent`:برای در اوردن کاربر از حالت /silent.
-        \n`/expire`:**برای مشاهده زمان باقی مانده از اشتراک شما.**
-        \n`/restore` [number] : برای خرید دوباره اشتراک میباشد.
         \n`/tag`: این دستور برای تگ کردن تمامی افراد گروه میباشد.
         \n`/Del`:اگر روی پیامی ریپلای شود اون پیام را پاک میکند.
         \n`/GSilent`:برای سکوت کردن کاربر در گروه میباشد.
@@ -108,15 +115,18 @@ async def callback(event):
         \n`/promote`:ادمین کردن کاربر برای انجام دستورات گروهی.
         \n`/demote`: برای عزل کردن کاربر از حالت ادمین
         \n`/Gmedia`:اگر روی یک ویدیو ریپلی شود ان را در حافظه سلف ذخیره و هر جایی که بخواهید میتوانید ان را ارسال کنید.
+        \n`/Ggit` [repo Link] : دانلود ریپازیتوری از گیتهاب
+        \n`/copycontent` [post Link] : پست های چنل هایی که فروارد آنها بسته است را سیو میکند
         '''
         back_button = Button.inline("ʙᴀᴄᴋ ⬸", b"Back")
         help_1 = Button.inline("ᴘᴀɢᴇ Ⅰ", b"help")
         help_2 = Button.inline("ᴘᴀɢᴇ Ⅱ", b"help_1")
         help_3 = Button.inline("ᴘᴀɢᴇ Ⅲ", b"help_2")
+        help_4 = Button.inline("ᴘᴀɢᴇ ɪᴠ", b"help_3")
         buttons = [
-            [help_1,help_2,help_3],
+            [help_1,help_2,help_3,help_4],
             [back_button]
-            ]
+        ]
         await event.edit(panel, buttons=buttons)
     elif event.sender_id == admin_user_id and event.data == b"help_2":
         panel = f'''
@@ -132,7 +142,6 @@ async def callback(event):
         \n`/randnum` [num1]-[num2]:ارسال عدد رندوم بین num1 و num2 .
         \n`/setname` [name]:اسم شما را عوض میکند.
         \n`/sfootball`:نشان دادن وضعیت تیم ها در Bundesliga.
-        \n`/azan` or `/اذان` [city]:اوقات شرعی شهر مورد نظر را نشان میدهد.
         \n`/setcolor` [green,pink,blue,red,yellow,purple,orange]:رنگ عکس ریپلای شده را به رنگ جلوی دستور تغییر میدهد.
         \n`/flood` [number] - [text1,text2,...]:به صورت رندوم به تعداد مشخص شده از بین کلمات تعیین شده پیام میفرستد.
         \n`/orcen` [ENtext] : متن را تبدیل به ویس میکند و میفرستد.
@@ -146,10 +155,49 @@ async def callback(event):
         help_1 = Button.inline("ᴘᴀɢᴇ Ⅰ", b"help")
         help_2 = Button.inline("ᴘᴀɢᴇ Ⅱ", b"help_1")
         help_3 = Button.inline("ᴘᴀɢᴇ Ⅲ", b"help_2")
+        help_4 = Button.inline("ᴘᴀɢᴇ ɪᴠ", b"help_3")
         buttons = [
-            [help_1,help_2,help_3],
+            [help_1,help_2,help_3,help_4],
             [back_button]
-            ]
+        ]
+        await event.edit(panel, buttons=buttons)
+    elif event.sender_id == admin_user_id and event.data == b"help_3":
+        panel = f'''
+        از دستورات زیر استفاده کنید:
+        \n`/time` [capital of country]: با قرار دادن پایتخت یک کشور زمان آن را به شما نشان می دهد
+        \n`newtimer` [name]: یک تایمر با نام دلخواه برای شما ایجاد میکند
+        \n`deltimer` [name]: تایمر مورد نظر را پاک می کند
+        \n`timers` : تایمر ها را نمایش میدهد
+        \n`clean timers` : تایمر ها را پاک میکند
+        \n`gfile` [link] : با قرار دادن لینک جلو دستور آن را دانلود میکند
+        \n`getip` [ip] : اطلاعات ip را میدهد
+        \n`/sunextract`یا `/استخراج فایل` : اگر روی یک فایل زیپ ریپلای شود آن را اکسترکت میکند
+        \n`Stv` : لینک پخش آنلاین تلویزیون
+        \n`sqr` یا `به qr` : تبدیل به qrcode
+        \n`readqr` یا `خواندن qr` : qrcode میخونه
+        \n`cleanall` یا `پاکسازی همه` [txt] : پیام مورد نظر را در گروه پاک میکند 
+        \n`setusername` [txt] : نام کاربری تنظیم میکند
+        \n`kick` [usernames] or [numerical user IDs] : یوزر مورد نظر از گروه کیک میکنه
+        \n`cleanb` [link 1] [link 2] : پیام های بین دو لینک پاک میکنه
+        \n`/rem` [num] - حذف پیام‌های اخیر در گروه تا تعداد مشخص شده.
+        \n`/sgoogle` [query] - جستجوی کوتاهی در گوگل برای پیدا کردن پاسخ به سوال شما.
+        \n`/wiki` [query] - جستجوی کوتاهی در ویکی‌پدیای فارسی برای پیدا کردن پاسخ به سوال شما.
+        \n`/save`: این دستور می‌تواند برای ذخیره یک گفتگو یا لاگ چت به یک فایل استفاده شود، که می‌تواند برای نگهداری یک رکورد از بحث‌های مهم یا به اشتراک گذاری با دیگران مفید باشد.
+        \n`/reload`: این دستور برای بازنشانی رابط گفتگو یا کد ربات استفاده می‌شود، که می‌تواند در صورت وجود هرگونه خطا یا خللی که باید حل شود مفید باشد.
+        \n`ReadAll`(Pvs-Gps-Channels-Bots): با توجه به مقداری که جلوش هست پیام های اونجا سین میکنه
+        \n`Del`(Videos-Photos-Voices-Files-Notes-Gifs): متغییر رو به روشو در اون چت همه شو پاک میکنه
+        \n`typing` [on|off]: اگه در پیوی کسی بفرستی برای اون شخص حالت تایپینگ میگیری
+        \n`Pvinfo`: اطلاعات اون چت میده
+        '''
+        back_button = Button.inline("ʙᴀᴄᴋ ⬸", b"Back")
+        help_1 = Button.inline("ᴘᴀɢᴇ Ⅰ", b"help")
+        help_2 = Button.inline("ᴘᴀɢᴇ Ⅱ", b"help_1")
+        help_3 = Button.inline("ᴘᴀɢᴇ Ⅲ", b"help_2")
+        help_4 = Button.inline("ᴘᴀɢᴇ ɪᴠ", b"help_3")
+        buttons = [
+            [help_1,help_2,help_3,help_4],
+            [back_button]
+        ]
         await event.edit(panel, buttons=buttons)
     elif event.sender_id == admin_user_id and event.data == b"turn_on":
         with open('settings/time.txt', 'w') as f:
